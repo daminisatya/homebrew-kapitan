cbf129f3d55853e86b3c72ea3f079fbd4521081e
dsatya <daminisatya@gmail.com>
2019-05-30 11:17:17 +0530

    update ruby script

--- kapitan.rb
+++ kapitan.rb
@@ -1,11 +1,11 @@
 class Kapitan < Formula
   desc "Generic templated configuration management for Kubernetes, Terraform and other things"
   homepage "https://kapitan.dev"
-  url "https://github.com/deepmind/kapitan/archive/v0.23.1.tar.gz"
-  sha256 "a8bc22e152c6c5ad5623a6c1cd35b74b81b682bce48dd4fef31600f322d4ff20"
+  url "https://files.pythonhosted.org/packages/5e/72/e43460e53e28a9656bce72017fd1c2c7c97721209f180ac62881244e3a8e/kapitan-0.23.1.tar.gz"
+  sha256 "551b4ab9610123f6952315c454eb1fd69ed85518d2be3727681dcab6dd16ba2e"
 
   def install
-    PYTHONPATH "kapitan"
+    system "python3.6 setup.py install"
   end
 
   test do
