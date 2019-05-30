9cd3267ad684eef8abdebafa5a3a90fe23142040
Damini Satya <daminisatya@gmail.com>
2019-05-30 11:32:12 +0530

    Update kapitan.rb

--- kapitan.rb
+++ kapitan.rb
@@ -3,9 +3,9 @@ class Kapitan < Formula
   homepage "https://kapitan.dev"
   url "https://files.pythonhosted.org/packages/5e/72/e43460e53e28a9656bce72017fd1c2c7c97721209f180ac62881244e3a8e/kapitan-0.23.1.tar.gz"
   sha256 "551b4ab9610123f6952315c454eb1fd69ed85518d2be3727681dcab6dd16ba2e"
-
+  
   def install
-    system ("python3.6", "setup.py", "install")
+    system "python3.6", "setup.py", "install"
   end
 
   test do
